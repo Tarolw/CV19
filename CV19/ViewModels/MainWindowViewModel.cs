86daa3d552b5a3f368acbce14f992c1b20693cc7
Богомолов Андрей <the.andrey.bogomolov@gmail.com>
2020-10-01 22:44:01 +0300

    Тестирование работы модели-представления главного окна

---
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using CV19.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CV19.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Заголовок окна
+
+        // Поле для свойства "Заголовок окна"
+        private string _Title = "Анализ статистики CV19";
+
+        /// <summary>Заголовок окна</summary>
+
+        // Свойство "Заголовок окна"
+        public string Title
+        {
+            get => _Title;
+            //set
+            //{
+            //    //if (Equals(_Title, value)) return;
+            //    //_Title = value;
+            //    //OnPropertyChanged();
+
+            //    Set(ref _Title, value);
+            //}
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
+    }
+}
