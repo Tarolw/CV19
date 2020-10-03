74ed46c20c6f87e651c679012a66ddfb0895fe34
Богомолов Андрей <the.andrey.bogomolov@gmail.com>
2020-10-03 19:16:43 +0300

    Базовый класс команды

---
+++ CV19/Infrastructure/Commands/Base/Command.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace CV19.Infrastructure.Commands.Base
+{
+    internal abstract class Command : ICommand
+    {
+        // вызывается в случае изменения состояния команды
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        /* Если возвращает ложь, то команду выполнить нельзя
+         * и элемент к которому привязана команда отключается
+         */
+        public abstract bool CanExecute(object parameter);
+
+        // Основная логика команды
+        public abstract void Execute(object parameter);
+    }
+}
