ebae699b8e8140edcccce39830d79670d7e5fd14
Богомолов Андрей <the.andrey.bogomolov@gmail.com>
2020-10-07 23:55:09 +0300

    Добавляем тестовые данные в модель-представления

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CV19.Infrastructure.Commands;
+using CV19.Models;
 using CV19.ViewModels.Base;
 using System;
 using System.Collections.Generic;
@@ -10,7 +11,21 @@ namespace CV19.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
-        #region Заголовок окна
+        #region TestDataPoints : IEnumerable<DataPoint> - Тестовый набор данных для визуализации графиков
+
+        ///<summary>Тестовый набор данных для визуализации графиков</summary>
+        private IEnumerable<DataPoint> _TestDataPoints;
+
+        ///<summary>Тестовый набор данных для визуализации графиков</summary>
+        public IEnumerable<DataPoint> TestDataPoints
+        {
+            get => _TestDataPoints;
+            set => Set(ref _TestDataPoints, value);
+        }
+
+        #endregion
+
+        #region Title : string - Заголовок окна
 
         /// <summary>Заголовок окна</summary>
         private string _Title = "Анализ статистики CV19";
@@ -32,7 +47,7 @@ namespace CV19.ViewModels
 
         #endregion
 
-        #region Статус программы
+        #region Status : string - Статус программы
 
         ///<summary>Статус программы</summary>
         private string _Status = "Готов!";
@@ -76,6 +91,17 @@ namespace CV19.ViewModels
                 CanCloseApplicationCommandExecute);
 
             #endregion
+
+            var data_points = new List<DataPoint>((int)(360 / 0.1));
+            for(var x = 0d; x <= 360; x += 0.1)
+            {
+                const double to_rad = Math.PI / 180;
+                var y = Math.Sin(x * to_rad);
+
+                data_points.Add(new DataPoint { XValue = x, YValue = y });
+            }
+
+            TestDataPoints = data_points;
         }
     }
 }
